30e40859082bb210684395ee93a2744f83791efd
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-08-17 18:05:04 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize (word)
+        @word = word
+    end
+    def match(arr)
+        matched = []
+        arr.map do |word|
+            if word.chars.sort == @word.chars.sort
+              matched.push(word)
+            
+            end
+        end
+        matched
+    end
+end
+
+
